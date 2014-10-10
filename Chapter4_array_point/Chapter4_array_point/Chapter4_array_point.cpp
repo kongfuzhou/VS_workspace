8986225fd3ea065c5ad6d5a6d0a4821778c98aa5
kongfuzhou <zhou136948247@126.com>
2014-10-10 10:46:42 +0800

    update
    update

--- Chapter4_array_point/Chapter4_array_point/Chapter4_array_point.cpp
+++ Chapter4_array_point/Chapter4_array_point/Chapter4_array_point.cpp
@@ -21,12 +21,32 @@ using std::array;
 
 void aboutPoint()
 {
+	//什么是指针:指针保存的是另一个对象的地址
+	//指针初始化或赋值时必须保证类型匹配
+	//C++ 提供了一种特殊的指针类型 void*，它可以保存任何类型对象的地址：
 	string str1("hell word");
 	string *pstr1=&str1;
 	cout<<*pstr1<<endl;
 	string str2="你好";
 	pstr1=&str2;
 	cout<<*pstr1<<endl;
+
+	int ival = 1024, ival2 = 2048;
+	int *pi = &ival, *pi2 = &ival2;
+	pi = pi2; // pi 和 pi2都是指向同一个地址（&ival2引用的地址）
+
+	cout<<"ival="<<ival<<" ival2="<<ival2<<" pi="<<pi<<" *pi="<<*pi<<" pi2="<<pi2<<" *pi2="<<*pi2<<endl;
+
+	//修改指针的值可以让指针指向另一对象 然后再解引用修改值 不会影响指针原来指向的值
+
+	int i_1=10;
+	int i_2=20;
+
+	int *p_1=&i_1;
+	int *p_2=&i_2;
+
+	p_1=p_2;//给指针重新赋值
+	*p_2=101;//给指针指向的对象重新赋值
 
 }
 
